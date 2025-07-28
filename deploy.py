aa6d7739ac3a89fcb1bb26a339f530b1febdce90
chewang07 <wangchen9617@gmail.com>
2025-07-29 03:58:55 +0800

    init commit

--- deploy.py
+++ deploy.py
@@ -1,8 +1,23 @@
+import sys
+import argparse
 from package_deploy import Deploy
+
+
+def parse_args(args):
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--deploy_type",
+        default="patch"
+    )
+    args = parser.parse_args(args)
+    return args
+
+run_args = parse_args(sys.argv[1:])
 
 
 class PKGDeploy(Deploy):
     project_name = 'package-deploy'
+    deploy_type = run_args.deploy_type
     cython = True
 
 
