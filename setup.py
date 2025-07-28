709407aa95b5ddc10a6c7e0c667544131d080ff4
chewang07 <wangchen9617@gmail.com>
2025-07-29 02:36:26 +0800

    Bump version: 0.0.5 → 0.0.6

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ with open("requirements.txt") as f:
 
 setup_kwargs: Dict[str, Any] = dict(
     name="package-deploy",
-    version="0.0.5",
+    version="0.0.6",
     author="Chen Wang",
     author_email="",
     description="Package deploy",
